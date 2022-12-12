041d7f68c8f4dc493f744f00b88bb2842a171f76
Иван Бояринцев <boyarintsev_i@GYMNASY.LOCAL>
2022-12-12 10:00:29 +0300

    little changes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from app.app_file import app
 from flask_restful import Resource, reqparse, abort
 from flask_jwt_simple import jwt_required, get_jwt_identity
-from flask import jsonify, request as req
+from flask import jsonify, request as req, render_template
 from app.models import HelpRequest
 from flask import Response
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 
 @app.route('/api/help_requests', methods=['GET', 'POST'])
@@ -16,53 +21,6 @@ def help_requests():
         app.help_repo.request_create(request)
         return jsonify(request)
 
-'''help_parser = reqparse.RequestParser()
-help_parser.add_argument('id', required=True)
-help_parser.add_argument('author', required=True)
-help_parser.add_argument('title')
-help_parser.add_argument('text')
-help_parser.add_argument('time_created')
-
-
-class HelpRes(Resource):
-    def get(self, post_id):
-        post = app.help_repo.get_by_id(post_id)
-        print(post)
-        return jsonify(post)
-
-    # :@jwt_required
-    def delete(self, id):
-        result = app.help_repo.request_delete(id)
-        if result:
-            abort(400, message=result)
-        return jsonify({"message": "success"})
-
-
-class HelpListRes(Resource):
-    def get(self, **kwargs):
-        if "category_name" in kwargs:
-            return jsonify(app.help_repo.get_by_category(kwargs["category_name"]))
-        posts = app.help_repo.get_all()
-        return jsonify(posts)
-
-    # @jwt_required
-    def post(self):
-        args = help_parser.parse_args()
-        post = HelpRequest(
-            category=args.get('category', None),
-            title=args.get('title', None),
-            text=args.get('text', None),
-            url=args.get('url', None),
-            type=args.get('type', None),
-        )
-        print("~~~~This worked!~~~~")
-        post = app.help_repo.request_create(post)
-        return jsonify(post)
-
-
-app.api.add_resource(HelpRes, '/api/help_request/<int:help_id>')
-app.api.add_resource(HelpListRes, '/api/help_requests/')
-'''
 
 if __name__ == '__main__':
     app.run()
