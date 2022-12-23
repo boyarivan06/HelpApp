968ad15f9693a29a2361420ff084c878566aa46d
Иван Бояринцев <boyarintsev_i@GYMNASY.LOCAL>
2022-12-23 16:09:40 +0300

    added web-interface

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 
-SECRET_KEY = ''
+SECRET_KEY = b'6)\xba\xdf\x8euz\x00\x0f\xd7\xaa\xdb\xb4\x8a\xd4\xbe\x98\x1b\x84C\xcfR\xce9WL\xd1f\xd2\x16\x16\xe5'
 
 STATIC_FOLDER = 'static'
 TEMPLATE_FOLDER = 'templates'
--- main.py
+++ main.py
@@ -4,18 +4,32 @@ from flask_jwt_simple import jwt_required, get_jwt_identity
 from flask import jsonify, request as req, render_template
 from app.models import HelpRequest
 from flask import Response
+from app.forms import HelpForm
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    form = HelpForm()
+    if form.validate_on_submit() or req.method == 'POST':
+        app.help_repo.request_create(HelpRequest(author='testing-testing...'))
+        print('.........HELP..........')
+        print(jsonify(app.help_repo.get_all()).json)
     return render_template('index.html')
+
+
+@app.route('/admin')
+def admin():
+    data = app.help_repo.get_all()
+    return render_template('admin.html', data=data)
 
 
 @app.route('/api/help_requests', methods=['GET', 'POST'])
 def help_requests():
     if req.method == 'GET':
         requests = app.help_repo.get_all()
-        return jsonify(requests)
+        answer = jsonify(requests)
+        print(answer.json)
+        return 'is it works?'
     elif req.method == 'POST':
         request = HelpRequest(author=req.json['author'])
         app.help_repo.request_create(request)
